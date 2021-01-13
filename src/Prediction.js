829b1c224eed3859ba0f726b8821dfee64d69355
Aigoshka <74876549+Aigoshka@users.noreply.github.com>
2021-01-13 10:13:46 +0100

    Changed col to px-0
    Boostrap overriding the col class so better to use px-0 which is also bootstrap class so local and netlify versions do look the same.

--- src/Prediction.js
+++ src/Prediction.js
@@ -8,7 +8,7 @@ export default function Prediction (){
 
 <h1 className="five-days-headline">Next 5 Days test</h1>
           <div className="row row-cols-5">
-            <div className="forecast col">
+            <div className="forecast px-0">
                 <div className="pred-style">
               <ul className="first-pred">
                   <li><span>Mon</span></li>
@@ -28,7 +28,7 @@ export default function Prediction (){
               </ul>
                 </div>
             </div>
-            <div className="forecast-1 col">
+            <div className="forecast-1 px-0">
               <div className="pred-style">
                 <ul>
                    <li><span>Tue</span></li>
@@ -48,7 +48,7 @@ export default function Prediction (){
                 </ul>
               </div>
             </div>
-            <div className="forecast-2 col">
+            <div className="forecast-2 px-0">
               <div className="pred-style">
                 <ul>
                     <li><span>Wed</span></li>
@@ -67,7 +67,7 @@ export default function Prediction (){
                 </ul>
               </div>
             </div>
-            <div className="forecast-3 col">
+            <div className="forecast-3 px-0">
                <div className="pred-style">
                 <ul>
                     <li><span>Thur</span></li>
@@ -86,7 +86,7 @@ export default function Prediction (){
                 </ul>
               </div>
             </div>
-            <div className="forecast-4 col">
+            <div className="forecast-4 px-0">
                <div className="pred-style">
                 <ul>
                     <li><span>Fri</span></li>
