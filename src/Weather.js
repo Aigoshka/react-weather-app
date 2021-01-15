4d9f258052e587d8c52739979c97779d74e87cd9
Aigoshka <74876549+Aigoshka@users.noreply.github.com>
2021-01-15 21:58:43 +0100

    Added Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
 import React,{useState} from "react";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
-import FormattedHours from "./FormattedHours"
+import WeatherInfo from "./WeatherInfo";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from "axios";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Weather(props) {
 let [weatherData, setWeatherData]=useState({ready:false});
-
+let [city, setCity]=useState (props.defaultCity);
 
 function displayTemperature(response){
   console.log (response.data);
@@ -23,15 +23,32 @@ description: response.data.weather[0].description,
 max: response.data.main.temp_max,
 date: new Date (response.data.dt*1000),
 
-  })
+  });
  
 }
+function search(){
+ let apiKey ="f36e45e370221a0b671266843fbab2eb";
+   
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+    function handleSubmit(event){
+    event.preventDefault();
+    search()
+    }
+    
+    function handleCityChange(event){
+      event.preventDefault();
+      setCity(event.target.value);
+    }
+
 if (weatherData.ready){
   return (
    <div className="Weather">
      <div className="row">
-        <form className="form-inline">
-          <input
+        <form onSubmit={handleSubmit} className="form-inline">
+          <input onChange={handleCityChange}
             type="text"
             className="form-control mb-2 mr-sm-2"
             placeholder="Enter a City"
@@ -46,96 +63,12 @@ if (weatherData.ready){
           </button>
         </form>
       </div>
-      <div className="row">
-        <div className="col-6 city-day">
-          <ul className="info">
-  <li className="city-headline">{weatherData.city}</li>
-  <li className="day-headline"><FormattedDate date={weatherData.date}/></li>
-          </ul>
-        </div>
-        <div className="col-6 hours"><FormattedHours date={weatherData.date}/></div>
-      </div>
-<div className="row current-weather">
-        <div className="col-6">
-          <ul>
-            <li>
-              <img
-                className="main-weather-icon"
-                src="images/rainy-2.svg"
-               alt="cloudy day"
-              />
-            </li>
-            <li className="main-icon-description">
-  {weatherData.description}
-            </li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <ul>
-              <li>
-  <span className="current-deegre">{Math.round(weatherData.temperature)}</span>
-                <span className="main-deegre-units">
-                  {" "}
-                 
-                  C˚ |
-                  F˚
-                </span>
-              </li>
-              <li>
-                <span className="feels-like">
-  Feels like <span>{Math.round(weatherData.feels)}</span>{" "}
-                  <span className="feels-like-unit">C˚| F˚</span>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="row description">
-        <div className="col-4 px-0">
-          <ul>
-            <li className="second-desc-style">Wind</li>
-            <li className="wind-box">
-            <FontAwesomeIcon icon="wind" className="wind" />
-
-            </li>
-            <li className="second-desc-style">
-  <span>{Math.round(weatherData.wind)}</span> km/h
-            </li>
-          </ul>
-        </div>
-        <div className="col-4 px-0">
-          <ul>
-            <li className="second-desc-style">Max Temp</li>
-            <li className="max-box">
-              <FontAwesomeIcon icon="temperature-high" className="high-temp"/>
-            </li>
-            <li className="second-desc-style">
-  <span>{Math.round(weatherData.max)}</span> C˚
-            </li>
-          </ul>
-        </div>
-        <div className="col-4 px-0">
-          <ul>
-            <li className="second-desc-style">Humidity</li>
-            <li className="humidity-box">
-              <FontAwesomeIcon icon="tint" className="tint"/>
-            </li>
-            <li className="second-desc-style">
-  <span>{weatherData.humidity}</span> %
-            </li>
-          </ul>
-        </div>
-      </div>
+      <WeatherInfo data={weatherData}/>
    </div>
 );}
 else {
-    let apiKey ="f36e45e370221a0b671266843fbab2eb";
-   
-    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-return "Loading ";
+  search();
+   return "Loading ";
 }
 }
 
---
+++ src/WeatherInfo.js
@@ -0,0 +1,93 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import FormattedHours from "./FormattedHours";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function WeatherInfo (props){
+    return (
+        <div className="WeatherInfo">
+        <div className="row">
+        <div className="col-6 city-day">
+          <ul className="info">
+  <li className="city-headline">{props.data.city}</li>
+  <li className="day-headline"><FormattedDate date={props.data.date}/></li>
+          </ul>
+        </div>
+        <div className="col-6 hours"><FormattedHours date={props.data.date}/></div>
+      </div>
+<div className="row current-weather">
+        <div className="col-6">
+          <ul>
+            <li>
+              <img
+                className="main-weather-icon"
+                src="images/rainy-2.svg"
+               alt="cloudy day"
+              />
+            </li>
+            <li className="main-icon-description">
+  {props.data.description}
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <ul>
+              <li>
+  <span className="current-deegre">{Math.round(props.data.temperature)}</span>
+                <span className="main-deegre-units">
+                  {" "}
+                 
+                  C˚ |
+                  F˚
+                </span>
+              </li>
+              <li>
+                <span className="feels-like">
+  Feels like <span>{Math.round(props.data.feels)}</span>{" "}
+                  <span className="feels-like-unit">C˚| F˚</span>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row description">
+        <div className="col-4 px-0">
+          <ul>
+            <li className="second-desc-style">Wind</li>
+            <li className="wind-box">
+            <FontAwesomeIcon icon="wind" className="wind" />
+
+            </li>
+            <li className="second-desc-style">
+  <span>{Math.round(props.data.wind)}</span> km/h
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 px-0">
+          <ul>
+            <li className="second-desc-style">Max Temp</li>
+            <li className="max-box">
+              <FontAwesomeIcon icon="temperature-high" className="high-temp"/>
+            </li>
+            <li className="second-desc-style">
+  <span>{Math.round(props.data.max)}</span> C˚
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 px-0">
+          <ul>
+            <li className="second-desc-style">Humidity</li>
+            <li className="humidity-box">
+              <FontAwesomeIcon icon="tint" className="tint"/>
+            </li>
+            <li className="second-desc-style">
+  <span>{props.data.humidity}</span> %
+            </li>
+          </ul>
+        </div>
+      </div>
+     </div>
+    );
+}
